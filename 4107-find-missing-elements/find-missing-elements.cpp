1458548f2b74119cb5a74a8cc5ab912bd9c15e0f
Luvneet Singh <Luvneetsingh3@gmail.com>
2026-08-04 21:17:54 +0530

    Time: 11 ms (6.06%) | Memory: 33.7 MB (17.56%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.cpp
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -5,14 +5,10 @@ public:
         for(int x:nums){
             m[x]++;
         }
+
+        sort(nums.begin(),nums.end());
         int min=nums[0];
-        int max=nums[0];
-
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]>max) max=nums[i];
-            if(nums[i]<min) min=nums[i];
-        }
-
+        int max=nums[nums.size()-1];
         vector<int>ans;
         
         for(int i=min;i<=max;i++){
