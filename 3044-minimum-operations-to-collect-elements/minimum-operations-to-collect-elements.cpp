5a50708166969a9b96e1b5d976f02cfda88a7938
Luvneet Singh <Luvneetsingh3@gmail.com>
2026-08-21 22:34:06 +0530

    Time: 4 ms (7.08%) | Memory: 23 MB (26.89%) - LeetSync

---
+++ 3044-minimum-operations-to-collect-elements/minimum-operations-to-collect-elements.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+    unordered_map<int,int>m;
+    for(int i=1;i<=k;i++){
+        m[i]++;
+        }
+
+        int ans=0;
+        for(int i=nums.size()-1;i>=0;i--){
+            ans++;
+           if(m.find(nums[i])!=m.end())
+             m[nums[i]]--;
+
+             if(m[nums[i]]==0)
+             m.erase(nums[i]);
+
+            if(m.empty())
+            break;
+        }
+        return ans;
+    }
+};
