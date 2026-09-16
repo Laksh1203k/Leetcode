edb29438c7b064fad60ac0c6b0122f4638fe1f5b
Luvneet Singh <Luvneetsingh3@gmail.com>
2026-09-16 21:26:25 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (75.02%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size())  return false;
+
+        vector<int>arr(26);
+
+        for(int i=0;i<s.size();i++){
+            arr[s[i]-'a']++;
+            arr[t[i]-'a']--;
+        }
+
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]!=0)
+            return false;
+        }
+        return true;
+    }
+};
